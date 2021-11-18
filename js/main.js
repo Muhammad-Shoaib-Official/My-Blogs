6d4ab21da22beb4c09921ea0c06ca1cf2579cbab
Muhammad Shoaib <94632204+Muhammad-Shoaib-Official@users.noreply.github.com>
2021-11-18 21:56:02 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var navLink = document.getElementById("navlink");
+navLink.style.right=' -250px';
+
+function showMenu(){
+    navLink.style.right=' 0px'; 
+}
+function hideMenu(){
+    navLink.style.right='-250px'; 
+}
